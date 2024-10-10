21ba7e757b87da3efb116128c5c680eda210d869
escolaestudante <105922788+escolaestudante@users.noreply.github.com>
2024-10-10 11:14:18 +0000

    we

--- graficos/informaçõesGlobais.js
+++ graficos/informaçõesGlobais.js
@@ -1,5 +1,7 @@
-const url = https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json
+const url = "https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json"
 
 async function visualizarInformaçõesGlobais(){
-    
+    const res = await fetch(url)
+    const dados = await res.json()
+    console.log(dados);
 }
