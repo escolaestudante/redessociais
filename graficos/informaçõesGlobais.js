6ccf52270788d4ad2c05da0bf1dff3239b738780
escolaestudante <105922788+escolaestudante@users.noreply.github.com>
2024-10-10 11:46:27 +0000

    er

--- graficos/informaçõesGlobais.js
+++ graficos/informaçõesGlobais.js
@@ -4,5 +4,13 @@ async function visualizarInformaçõesGlobais(){
     const res = await fetch(url)
     const dados = await res.json()
     console.log(dados);
+    const paragrafo = document.createElement('p')
+    paragrafo.classList.adda('graficos-container_texto')
+    paragrafo.innerHTML = 'você sabia que o mundo tem ${dados.tota._pessoas_mundo} ${de pessoaas e que aproximadamente ${dados.total_pessoas_conectadas} e estao conectados em alguma rede social e passam em media ${dados.tempo_medio} horas conectadas.'
+
+    const container = document.getElementById('graficos-container')
+    container.appendChild(paragrafo)
+
+
 }
 visualizarInformaçõesGlobais()
