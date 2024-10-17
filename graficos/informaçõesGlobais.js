22b0b6e685014b93cd1b4a45ae5170675ded4d76
escolaestudante <105922788+escolaestudante@users.noreply.github.com>
2024-10-17 08:46:02 -0300

    sewr

--- graficos/informaçõesGlobais.js
+++ graficos/informaçõesGlobais.js
@@ -3,11 +3,13 @@ const url = "https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-g
 async function visualizarInformaçõesGlobais(){
     const res = await fetch(url)
     const dados = await res.json()
-    console.log(dados);
+    const pessoasConectadas = (dados.total_pessoas_concetadas/1e9)
+    const pessoasNoMundo= (dados.total_pessoas_mundo/1e9)
+    const horas=parseInt(dados.tempo_medio)
+    const porcentagemConectada=((pessoasConectadas/pessoasNoMundo)*100).toFixed(2);
     const paragrafo = document.createElement('p')
     paragrafo.classList.adda('graficos-container_texto')
     paragrafo.innerHTML = 'você sabia que o mundo tem ${dados.tota._pessoas_mundo} ${de pessoaas e que aproximadamente ${dados.total_pessoas_conectadas} e estao conectados em alguma rede social e passam em media ${dados.tempo_medio} horas conectadas.'
-
     const container = document.getElementById('graficos-container')
     container.appendChild(paragrafo)
 
